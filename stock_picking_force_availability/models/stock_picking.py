089b00f5c1d83728f846d5d4987149c8833ba5dd
angel <angel.garcia@sygel.com>
2023-04-14 14:07:36 +0200

    [15.0][IMP] stock_picking_force_availability: allow to force availability by operation type

--- stock_picking_force_availability/models/stock_picking.py
+++ stock_picking_force_availability/models/stock_picking.py
@@ -1,11 +1,16 @@
 # Copyright 2023 Ángel García de la Chica Herrera <angel.garcia@sygel.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import models, fields
 
 
 class Picking(models.Model):
     _inherit = "stock.picking"
+
+    allow_force_availability = fields.Boolean(
+        related="picking_type_id.allow_force_availability",
+        string="Allow to Force Availability",
+    )
 
     def action_force_availability(self):
         self.ensure_one()
@@ -18,3 +23,12 @@ class Picking(models.Model):
                 lambda x: x.product_id.type == "product" 
                 and x.product_id.tracking != "none"):
             self.update({'state': 'assigned'})
+
+
+class PickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    allow_force_availability = fields.Boolean(
+        default=False,
+        string="Allow to Force Availability",
+    )
