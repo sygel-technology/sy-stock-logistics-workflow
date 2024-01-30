b3147db85cccee603721e73aa9a6046e9417eafa
angel <angel.garcia@sygel.com>
2024-01-30 16:31:01 +0100

    [NEW] stock_picking_report_line_product_without_internal_ref: hides the internal references of the products in the lines of the Stock Picking reports.

---
+++ stock_picking_report_line_product_without_internal_ref/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Ángel García de la Chica <angel.garcia@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Picking Report Line Product Without Internal Reference",
+    "summary": "Stock Picking Report Line Product Without Internal Reference",
+    "version": "15.0.1.0.0",
+    "category": "Stock",
+    "website": "https://www.sygel.es",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        'stock'
+    ],   
+    "data": [
+        'reports/report_deliveryslip.xml'
+    ],
+}
