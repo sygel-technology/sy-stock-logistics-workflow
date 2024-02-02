5e73c06df6fb3ab97a0d0eda293353908c984425
angel <angel.garcia@sygel.com>
2024-02-02 14:19:01 +0100

    [NEW] stock_picking_terms: Terms and conditions in stock picking.

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ Available addons
 ----------------
 addon | version | summary
 --- | --- | ---
+[stock_picking_terms](stock_picking_terms/) | 16.0.1.0.0 | Terms and conditions in stock picking.
 
 [//]: # (end addons)
---
+++ stock_picking_terms/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2024 Ángel García de la Chica Herrera <angel.garcia@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import company
+from . import res_config_settings
+from . import stock_picking
