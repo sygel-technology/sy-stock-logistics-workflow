62be1f2325b4f7e457c28fd5ebe36e7a96aa362f
Manuel Florido <manuel.florido@sygel.es>
2024-07-11 13:06:28 +0200

    [T-5944][ADD] First commit

--- stock_picking_terms/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2024 Ángel García de la Chica Herrera <angel.garcia@sygel.es>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import company
-from . import res_config_settings
-from . import stock_picking
