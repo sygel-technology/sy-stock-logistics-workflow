33ce1dbf43ab2420c0de93351d12f79bfb145a08
Manuel Florido <manuel.florido@sygel.es>
2024-07-11 12:44:21 +0200

    [T-5944][IMP]picking_type_confirmation_requirement_rules,picking_very_high_priority,stock_picking_force_availability,stock_picking_report_line_product_without_internal_ref,stock_picking_show_move_lots:pre-commit auto fixes

--- stock_picking_report_line_product_without_internal_ref/__manifest__.py
+++ stock_picking_report_line_product_without_internal_ref/__manifest__.py
@@ -6,15 +6,11 @@
     "summary": "Stock Picking Report Line Product Without Internal Reference",
     "version": "15.0.1.0.0",
     "category": "Stock",
-    "website": "https://www.sygel.es",
+    "website": "https://github.com/sygel-technology/sy-stock-logistics-workflow",
     "author": "Sygel, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        'stock'
-    ],   
-    "data": [
-        'reports/report_deliveryslip.xml'
-    ],
+    "depends": ["stock"],
+    "data": ["reports/report_deliveryslip.xml"],
 }
