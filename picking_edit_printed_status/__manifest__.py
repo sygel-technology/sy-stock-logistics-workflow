4ccd15f41dee1b9be5c737a64a191e0798fde2b8
sygel <alberto.martinez@sygel.es>
2024-12-05 23:06:25 +0100

    [T-6999][ADD] picking_edit_printed_status

---
+++ picking_edit_printed_status/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Alberto Martínez <alberto.martinez@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Picking Edit Printed Status",
+    "summary": "Edit printed status on pickings",
+    "version": "16.0.1.0.0",
+    "category": "Stock",
+    "website": "https://github.com/sygel-technology/sy-stock-logistics-workflow",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+}
---
+++ picking_edit_printed_status/readme/USAGE.md
@@ -0,0 +1,5 @@
+To use this module, you need to:
+
+1. Go to Inventory / Operations / Receipts or Deliveries
+2. On the tree view, click on the settings icon and note that you can add the optional printed field.
+3. Go to the form view of a picking. In the aditional info tab you can edit the printed field.
