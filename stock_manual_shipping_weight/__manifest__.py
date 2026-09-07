354cc5efe1b0ff36561e45448212f3b2f1acd3cd
Angel Rivas <angel.rivas@sygel.es>
2026-09-07 11:40:08 +0200

    [T-10155][IMP] stock_manual_shipping_weight: pre-commit auto fixes

--- stock_manual_shipping_weight/__manifest__.py
+++ stock_manual_shipping_weight/__manifest__.py
@@ -2,20 +2,20 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Stock Manual Shipping Weight',
-    'version': '14.0.1.0.1',
-    'author': 'Sygel',
-    'category': 'Stock',
-    'summary': 'The shipping weight field in pickings can be manually edited.',
-    'website': 'https://www.sygel.es',
-    'depends': [
-        'sale_stock',
-        'sale_management',
-        'delivery',
+    "name": "Stock Manual Shipping Weight",
+    "version": "14.0.1.0.1",
+    "author": "Sygel",
+    "category": "Stock",
+    "summary": "The shipping weight field in pickings can be manually edited.",
+    "website": "https://github.com/sygel-technology/sy-stock-logistics-workflow",
+    "depends": [
+        "sale_stock",
+        "sale_management",
+        "delivery",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/stock_picking_view.xml',
-        'wizard/edit_shipping_weight_wizard_view.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/stock_picking_view.xml",
+        "wizard/edit_shipping_weight_wizard_view.xml",
     ],
 }
